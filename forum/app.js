920743cbd6531ccb20f4e62725998e909ee82923
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-04-10 22:01:28 -0400

    change order of js

--- forum/app.js
+++ forum/app.js
@@ -6,11 +6,33 @@ function theFreelancers ()
     const freelancersList = document.getElementById('freelancersList');
 
 }
-const names = ['Alice', 'Bob', 'Carol', 'Julia', 'Emma',''];
-const occupations = ['Writer', 'Teacher', 'Programmer', 'Housekeeping', 'Personal Asst.'];
- 
-// let freelancers = [{
-    // name:
-// }]
+const freelancers = [
+    { name: 'Alice', job: 'Writer', price: 30 },
+    { name: 'Alice', job: 'Teacher', price: 30 },
+    { name: 'Bob', job: 'Teacher', price: 30 },
+    { name: 'Carol', job: 'Programmer', price: 70 },
+    { name: 'Julia', job: 'Housekeeping', price: 25 },
+    { name: 'Emma', job: 'Personal Assistant', price: 40 },
+    { name: 'Rick', job: 'Landscaper', price: 55 }
+];
+// let freelancers = []
+function getFreelancers ()
+{
+    const freelancersList = document.getElement
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log('hey');
