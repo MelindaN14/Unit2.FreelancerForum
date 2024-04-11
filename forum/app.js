b04a9bd5d07fc57c7393ba73f2b579116adca512
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-04-10 21:37:28 -0400

    saves

--- forum/app.js
+++ forum/app.js
@@ -3,8 +3,14 @@
 // must use DOM ? wth
 function theFreelancers ()
 {
-    const freelancersList = document.getElementById('')
+    const freelancersList = document.getElementById('freelancersList');
+
 }
 const names = ['Alice', 'Bob', 'Carol', 'Julia', 'Emma',''];
 const occupations = ['Writer', 'Teacher', 'Programmer', 'Housekeeping', 'Personal Asst.'];
  
+// let freelancers = [{
+    // name:
+// }]
+
+console.log('hey');
